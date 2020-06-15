9338819c64c272a8357d8c190642a908673f4a76
Filimon Mikhail <misafilimon255@mail.ru>
2020-06-16 00:22:13 +0300

    Выполнил 1 задание

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
-console.log('hello');
-let f = 5;
-console.log(f);
+let mone = 1,
+    incom = 2,
+    addExpense = 3,
+    deposi = 4,
+    missio = 5,
+    perio = 6;
+
+    console.log('Добро пожаловать в консоль');
+
+    alert('«Крик души» У меня кажется получилось выполнить урок!');
