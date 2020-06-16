b93f734403ffbed894ef023f9c53211f82f74b4b
Filimon Mikhail <misafilimon255@mail.ru>
2020-06-16 15:04:55 +0300

    lesson02(script.js)Deleted a comment

--- js/script.js
+++ js/script.js
@@ -22,26 +22,3 @@ addExpenses = addExpenses.toLowerCase();
 
     const budgetDay = money / 30;
     
-
-    // console.log('Добро пожаловать в консоль');
-
-    // const cor = 4**2; // современный синтаксис ES 7 (возведение в степень)
-
-    // console.log(cor); // возведение в степень новый формат
-    // console.log(Number.isInteger(5));// определяет целое ли число (true)
-    // console.log(Number.isInteger(5.5)); // определяет целое ли число (false)
-
-    // const str = 'Welcome, My Friend!';
-
-    // console.log(str[0], str[1]);
-    // console.log(str.toUpperCase()); // высший регистр
-    // console.log(str.toLowerCase()); // нижний регистр
-
-    // console.log(str.replace('Welcome', 'Good buy')); // замена одного текста на другой
-
-    // console.log(str.split(' ')); 
-    // Каждое слово становится отдельным массивом, можно сделать так и каждкую букву
-
-    // const strFriut = 'apple, kiwi, orange';
-
-    // console.log(strFriut.split(',')); // разбираем на массивы строку
